e687b0fa1f2180f33895816a479fc65c9e20c8a7
Lifeistrange <1505586488@qq.com>
2016-12-11 08:31:17 +0800

    引入flask-moment与flask-wtf

--- hello.py
+++ hello.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from datetime import datetime
+
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    return render_template('index.html', current_time=datetime.utcnow(), form=form)
 
 @app.route('/user/<name>')
 def user(name):
