8c6ff469253f99df8b2286bb599d7d25607d86b5
Lifeistrange <1505586488@qq.com>
2016-12-17 22:08:59 +0800

    修改个人资料页面

--- app/main/views.py
+++ app/main/views.py
@@ -3,11 +3,11 @@
 
 from datetime import datetime
 
-from flask import render_template, session, redirect, url_for
-from flask_login import login_required
+from flask import render_template, session, redirect, url_for, flash
+from flask_login import login_required, current_user
 
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..domain.model import User, Permission
 from ..domain.decorators import permission_required, admin_required
@@ -40,7 +40,7 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    return render_template('user.html', user=user)
+    return render_template('user.html', user=user, current_user=current_user)
 
 @main.route('/admin')
 @login_required
@@ -53,4 +53,20 @@ def for_admins_only():
 @permission_required(Permission.MODERATE_COMMENTS)
 def for_moderators_only():
     return "For comment"
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
